4de9f5248eb8f9bed850bc8397d3aa95b21b4cf1
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2023-01-08 15:26:26 +0100

    Create Enabling_WSL_Windows.ps1

---
+++ Enabling_WSL_Windows.ps1
@@ -0,0 +1,13 @@
+#Enable the Optional Feature
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+#All in one also with hyper-v (optional)
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform,Microsoft-Windows-Subsystem-Linux,Microsoft-Hyper-V-All
+
+#After restarting, open PowerShell (or Windows Terminal) with elevated privileges
+wsl --update
+
+#Set the default version
+wsl --set-default-version 2
