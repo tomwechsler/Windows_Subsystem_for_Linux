e1578aec30573f095d1d65a8c750c186b05c5beb
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2023-01-08 15:38:23 +0100

    Create Using_the_wsl.ps1

---
+++ Using_the_wsl.ps1
@@ -0,0 +1,36 @@
+#Open an PowerShell Session or the Windows Terminal
+
+#Get the help
+wsl --help
+
+#List the distribution
+wsl -l
+
+#List the distribution with more info
+wsl -l -v
+
+#To change the wsl version on a specific distribution
+wsl --set-version "Ubuntu" 1
+
+#Set the default wsl version
+wsl --set-default-version 2
+
+#Using linux commands
+wsl uname -a
+
+dir | wsl wc -l
+
+#Set the default distribution
+wsl --set-default "Ubuntu"
+
+#Check
+wsl -l -v
+
+#Run a command against a distribution
+wsl -d "Ubuntu" ls /
+
+#Run a command against a distribution (with username - it is not needed)
+wsl -d "Ubuntu" -u tom ls /
+
+#Uses the default distribution
+wsl ls /
