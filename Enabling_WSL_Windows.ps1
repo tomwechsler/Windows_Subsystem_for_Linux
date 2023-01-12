1093385da97e445c1401ad9c1571d3e8eb93c5d3
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2023-01-12 11:37:39 +0100

    Update Enabling_WSL_Windows.ps1

--- Enabling_WSL_Windows.ps1
+++ Enabling_WSL_Windows.ps1
@@ -1,4 +1,4 @@
-#Enable the Optional Feature
+#Enable the Optional Feature (Windows 10/11)
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
@@ -6,7 +6,7 @@ Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 #All in one also with hyper-v (optional)
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform,Microsoft-Windows-Subsystem-Linux,Microsoft-Hyper-V-All
 
-#Another install option (this installs wsl with the ubuntu distro)
+#Another install option for Windows Server/Client (this installs wsl with the ubuntu distro)
 wsl --install
 
 #Choose the distro
