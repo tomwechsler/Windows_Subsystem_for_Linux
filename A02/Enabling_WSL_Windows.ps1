7f30d36a38aef9eb53fb25f6183647fb61103ea1
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2023-05-24 16:31:45 +0200

    Update Enabling_WSL_Windows.ps1

--- A02/Enabling_WSL_Windows.ps1
+++ A02/Enabling_WSL_Windows.ps1
@@ -1,13 +1,15 @@
 #Enable the Optional Feature (Windows 10/11)
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
-
-#All in one also with hyper-v (optional)
-Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform,Microsoft-Windows-Subsystem-Linux,Microsoft-Hyper-V-All
 
 #Another install option for Windows Server/Client (this installs wsl with the ubuntu distro)
 wsl --install
+
+#Without a distribution
+wsl --install --no-distribution #no default distribution
+
+#Or via Microsoft Store
+https://www.microsoft.com/store/productId/9P9TQF7MRM4R
+
 
 #If on the Windows Server the ubuntu installation fails, try:
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -Out Microsoft.VCLibs.x64.14.00.Desktop.appx
