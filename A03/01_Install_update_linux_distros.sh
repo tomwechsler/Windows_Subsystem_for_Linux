da6799257237d0d3b764e67aff084026a3b0e385
Tom Wechsler <tom.wechsler@outlook.com>
2023-05-28 16:11:32 +0200

    lines edited

--- A03/01_Install_update_linux_distros.sh
+++ A03/01_Install_update_linux_distros.sh
@@ -3,6 +3,16 @@ wsl --list --online
 
 wsl --install -d "distro name"
 
+#Can change the default
+wsl --set-default Ubuntu
+
+#Terminate specific distribution
+wsl -t Ubuntu
+#Shutdown WSL (all)
+wsl --shutdown
+
+wsl --list --verbose
+
 #Update the ubuntu distro
 sudo apt update && sudo apt upgrade -y
 
--- A04/01_Using_the_wsl.ps1
+++ A04/01_Using_the_wsl.ps1
@@ -21,7 +21,9 @@ wsl --set-default-version 2
 #Using linux commands
 wsl uname -a
 
-dir | wsl wc -l
+wsl uptime -p #pretty
+
+wsl cat /etc/os-release
 
 #Set the default distribution
 wsl --set-default "Ubuntu"
@@ -37,3 +39,15 @@ wsl -d "Ubuntu" -u tom ls /
 
 #Uses the default distribution
 wsl ls /
+
+#Systemd support for AlmaLiunx
+sudo cat /etc/wsl.conf
+<#ADD
+[boot]
+systemd=true
+#>
+sudo cat /etc/wsl.conf
+
+wsl -t Ubuntu #from command then restart
+
+wsl -d "AlmaLinux9" sudo systemctl status
