90538ac31a61364bb0840d0f551a6f378837a043
Tom Wechsler <tom.wechsler@outlook.com>
2023-05-29 14:30:01 +0200

    lines edited

--- A04/01_Using_the_wsl.ps1
+++ A04/01_Using_the_wsl.ps1
@@ -39,15 +39,3 @@ wsl -d "Ubuntu" -u tom ls /
 
 #Uses the default distribution
 wsl ls /
-
-#Systemd support for AlmaLiunx
-sudo cat /etc/wsl.conf
-<#ADD
-[boot]
-systemd=true
-#>
-sudo cat /etc/wsl.conf
-
-wsl -t Ubuntu #from command then restart
-
-wsl -d "AlmaLinux9" sudo systemctl status
--- A04/02_Distribution_configuration_wsl.conf.sh
+++ A04/02_Distribution_configuration_wsl.conf.sh
@@ -23,3 +23,15 @@ wsl --shutdown
 
 #If you would configure settings for all distributions use the .wslconfig file:
 https://learn.microsoft.com/en-us/windows/wsl/wsl-config
+
+#Systemd support for AlmaLiunx
+sudo cat /etc/wsl.conf
+<#ADD
+[boot]
+systemd=true
+#>
+sudo cat /etc/wsl.conf
+
+wsl -t Ubuntu #from command then restart
+
+wsl -d "AlmaLinux9" sudo systemctl status
