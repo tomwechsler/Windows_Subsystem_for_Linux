267b7c31790172a91277d9ea4b54ea75e35ad50e
Tom Wechsler <tom.wechsler@outlook.com>
2023-06-06 15:12:45 +0200

    new files

---
+++ A05/04_Managing_Software_zypper.sh
@@ -0,0 +1,19 @@
+zypper                  #to display a list of available global options and commands.
+
+zypper help             #help search to display the help for the command search.
+
+zypper lp               #to see which patch updates are needed.
+
+zypper patch            #to apply the necessary patches.
+
+zypper se sqlite        #to search for sqlite.
+
+zypper rm sqlite2       #to remove sqlite2.
+
+zypper in sqlite3       #to install sqlite3.
+
+zypper in yast*         #to install all packages matching 'yast*'.
+
+zypper up               #to update all installed packages with newer versions where possible.
+
+zypper dup              #to perform a distribution upgrade (this is the recommended update method for openSUSE Tumbleweed).
