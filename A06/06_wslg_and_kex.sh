33b813fc1f49c21b7585ea51ba358a8631f4ba13
Tom Wechsler <tom.wechsler@outlook.com>
2023-06-11 18:42:38 +0200

    lines edited

---
+++ A06/06_wslg_and_kex.sh
@@ -0,0 +1,17 @@
+#On ubuntu
+sudo apt update && sudo apt install -y firefox
+
+firefox &
+
+#On Windows
+wslg firefox
+
+#Choose the distro
+wsl --list --online
+
+wsl --install -d "distro name"
+
+#On kali
+sudo apt install kali-win-kex -y
+
+kex
